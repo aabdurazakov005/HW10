f99a13077431e867147513262bba50b58e26b51c
aabdurazakov005 <aabdurazakov005@yandex.ru>
2024-07-18 14:20:46 +0300

    Доделанное домашнее задание 10.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanivich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println(" ");
+
+        System.out.println("Task 2");
+        System.out.println(fullName.toUpperCase());
+        System.out.println(" ");
+
+        System.out.println("Task 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.println(fullName1);
     }
 }
